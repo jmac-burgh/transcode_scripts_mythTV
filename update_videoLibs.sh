51bc0195d1b261d21589c97a133b8f8421c6f1c5
mckeever-home <mm_half3@yahoo.com>
2015-04-18 23:47:04 -0400

    modified video library update script

--- update_videoLibs.sh
+++ update_videoLibs.sh
@@ -8,8 +8,7 @@ export PATH=/bin:/usr/bin:/usr/sbin
 #
 
 xbmcUserName="kodi" # xbmc username
-xbmcPassword="apollo13" # xbmc password
-#xbmcIpAddress="192.168.0.16" # ip address of the machine that is running xbmc
+xbmcPassword="your password" # xbmc password
 xbmc_hosts=$1
 
 xbmcPort="8080" # the port that xbmc is listening on
@@ -31,27 +30,3 @@ cat $xbmc_hosts|while read xbmcIpAddress; do
 	/usr/bin/curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}' -H 'content-type: application/json;' $xbmcUserName:$xbmcPassword@$xbmcIpAddress:$xbmcPort/jsonrpc
 
 done
-
-#
-# lines that can be used to automate xbmc video library refresh to automatically add/delete items in video library using json
-# add any xbmc clients with a video library in the variable definition section above then uncomment lines below
-#
-
-# clean the xbmc video library if any links were removed
-#if [ "$linkDeleted" == "true" ]; then
-#    curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Clean", "id": "mybash"}' -H 'content-type: application/json;' $xbmcUserName:$xbmcPassword@$xbmcIpAddress:$xbmcPort/jsonrpc
-#    $(log $logFile "library clean request issued to xbmc")
-#    $(log $logFile "sleeping for 3 minutes while library is cleaned")
-#    sleep 3m
-#fi
-
-
-# update xbmc video library if any hard links were added.
-#if [ "$hardLinkCreated" == "true" ]; then
-#if [ "$hardLinkCreated" == "$hardLinkCreated" ]; then
-#    curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}' -H 'content-type: application/json;' $xbmcUserName:$xbmcPassword@$xbmcIpAddress:$xbmcPort/jsonrpc
-#    $(log $logFile "library scan request issued to xbmc")
-#    $(log $logFile "sleeping for 1 minute while library scans")
-#    sleep 1m
-#fi
-
